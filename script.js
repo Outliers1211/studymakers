f876156b5e0c4dfc8cfef15d020ac2177168beb8
rococop <rococop0624@gmail.com>
2025-06-07 10:39:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function showMessage() {
   document.getElementById("message").textContent = "스터디메이커즈에 오신 것을 환영합니다!";
 }
 
-// ✅ 사용자가 입력한 값을 처리하는 함수
+// 사용자가 입력한 값을 처리하는 함수
 function handleInput() {
   const input = document.getElementById("userInput").value.trim();
 
